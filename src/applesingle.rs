b73c89995ae852abebecb49821866a56f725f7ca
Jordan Yelloz <jordan@yelloz.me>
2021-12-23 15:44:00 -0800

    Added representation of applesingle v2 archives.
    At the moment it can only check the magic number and figure out the filename and
    comment.

---
+++ src/applesingle.rs
@@ -0,0 +1,228 @@
+use std::{
+    collections::BTreeMap,
+    io::{
+        self,
+        prelude::*,
+    },
+};
+
+use num_enum::{TryFromPrimitive, IntoPrimitive};
+
+use super::{
+    archive::Archive,
+    DataFork,
+    ResourceFork,
+    Filename,
+    Comment,
+    Dates,
+    FinderInfo,
+    MacInfo,
+};
+
+const MAGIC: u32 = 0x0005_1600;
+const VERSION: u32 = 0x0002_0000;
+
+const FORMAT_NAME: &str = "AppleSingle";
+
+#[derive(
+    Debug,
+    PartialEq, Eq,
+    PartialOrd, Ord,
+    TryFromPrimitive, IntoPrimitive,
+)]
+#[repr(u32)]
+enum EntryType {
+    DataFork = 1,
+    ResourceFork,
+    RealName,
+    Comment,
+    IconBW,
+    IconColor,
+    FileDates = 8,
+    FinderInfo,
+    MacintoshFileInfo,
+    ProDOSFileInfo,
+    MSDOSFileInfo,
+    AFPShortName,
+    AFPFileInfo,
+    AFPDirectoryID,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Segment {
+    pub id: u32,
+    pub offset: u32,
+    pub len: u32,
+}
+
+#[derive(Default)]
+struct ArchiveHeader {
+    segments: BTreeMap<u32, Segment>,
+}
+
+impl ArchiveHeader {
+    fn segments_by_offset(&self) -> Vec<Segment> {
+        let mut segments: Vec<Segment> = self.segments.values()
+            .cloned()
+            .collect();
+        segments.sort_by_key(|s| s.offset);
+        segments
+    }
+}
+
+#[derive(Debug)]
+pub enum ArchiveMember {
+    DataFork(DataFork),
+    ResourceFork(ResourceFork),
+    RealName(Filename),
+    Comment(Comment),
+    FileDates(Dates),
+    FinderInfo(FinderInfo),
+    MacInfo(MacInfo),
+    Other(Segment),
+}
+
+// TODO: Impl Read for this to simplify offset bookkeeping.
+struct AppleSingleArchiveReader<R: Read> {
+    reader: R,
+    header: ArchiveHeader,
+    position: usize,
+}
+
+impl <R: Read> AppleSingleArchiveReader<R> {
+    fn new(reader: R) -> io::Result<Self> {
+        let mut archive = Self {
+            reader,
+            header: ArchiveHeader::default(),
+            position: 0,
+        };
+        archive.read_magic()?;
+        archive.read_header()?;
+        Ok(archive)
+    }
+    fn read_magic(&mut self) -> io::Result<()> {
+        let Self { reader, position, .. } = self;
+        let mut bytes = [0u8; 4];
+        reader.read_exact(&mut bytes)?;
+        if u32::from_be_bytes(bytes) != MAGIC {
+            eprintln!("invalid magic: {:?}", &bytes);
+            let e: Result<Self, io::Error> = Err(io::ErrorKind::Other.into());
+            e?;
+        }
+        let mut bytes = [0u8; 4];
+        reader.read_exact(&mut bytes)?;
+        if u32::from_be_bytes(bytes) != VERSION {
+            eprintln!("invalid version: {:?}", &bytes);
+            let e: Result<Self, io::Error> = Err(io::ErrorKind::Other.into());
+            e?;
+        }
+        *position += 8;
+        Ok(())
+    }
+    fn read_header(&mut self) -> io::Result<()> {
+        let mut gap = [0u8; 16];
+        self.reader.read_exact(&mut gap)?;
+        let mut bytes = [0u8; 2];
+        self.reader.read_exact(&mut bytes)?;
+        let n_segments = u16::from_be_bytes(bytes);
+        self.position += gap.len() + bytes.len();
+        for _ in 0..n_segments {
+            self.read_segment()?;
+        }
+        Ok(())
+    }
+    fn read_segment(&mut self) -> io::Result<()> {
+        let Self { reader, header, position, .. } = self;
+        let mut bytes = [0u8; 4];
+        reader.read_exact(&mut bytes)?;
+        let id = u32::from_be_bytes(bytes);
+        reader.read_exact(&mut bytes)?;
+        let offset = u32::from_be_bytes(bytes);
+        reader.read_exact(&mut bytes)?;
+        let len = u32::from_be_bytes(bytes);
+        header.segments.insert(id, Segment { id, offset, len });
+        *position += 12;
+        Ok(())
+    }
+    fn segments(self) -> SegmentIterator<R> {
+        let segments = self.header.segments_by_offset();
+        SegmentIterator(
+            self.reader,
+            self.position,
+            Box::new(segments.into_iter()),
+        )
+    }
+}
+
+struct SegmentIterator<R>(R, usize, Box<dyn std::iter::Iterator<Item=Segment>>);
+
+impl <R: Read> SegmentIterator<R> {
+    fn skip_to(&mut self, segment: Segment) -> io::Result<()> {
+        let Self(stream, position, _) = self;
+        let offset = segment.offset as usize;
+        if *position > offset {
+            Err(io::ErrorKind::Unsupported)?;
+        }
+        let diff = (offset - *position) as u64;
+        let mut take = stream.take(diff);
+        io::copy(&mut take, &mut io::sink())?;
+        *position = offset;
+        Ok(())
+    }
+    fn wrap_segment(&mut self, segment: Segment) -> Option<ArchiveMember> {
+        eprintln!("wrapping segment {:?}", &segment);
+        self.skip_to(segment).expect("failed to seek");
+        let Self(stream, position, _) = self;
+        match EntryType::try_from(segment.id) {
+            Err(_) => Some(ArchiveMember::Other(segment)),
+            Ok(EntryType::RealName) => {
+                let len = segment.len as usize;
+                let mut buf = Vec::with_capacity(len);
+                buf.resize(len, 0);
+                stream.read_exact(&mut buf).expect("failed to read filename");
+                *position += len;
+                Some(ArchiveMember::RealName(Filename(buf)))
+            },
+            Ok(EntryType::Comment) => {
+                let len = segment.len as usize;
+                let mut buf = Vec::with_capacity(len);
+                buf.resize(len, 0);
+                stream.read_exact(&mut buf).expect("failed to read comment");
+                *position += len;
+                Some(ArchiveMember::Comment(Comment(buf)))
+            },
+            Ok(id) => {
+                eprintln!(
+                    "unsupported archive member type: {:?}, {:?}",
+                    id,
+                    segment,
+                );
+                None
+            }
+        }
+    }
+}
+
+impl <R: Read> Iterator for SegmentIterator<R> {
+    type Item = ArchiveMember;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let segment = self.2.next()?;
+            if let Some(member) = self.wrap_segment(segment) {
+                return Some(member);
+            }
+        }
+    }
+}
+
+pub fn parse<R: Read>(archive: R) -> io::Result<Archive> {
+    let reader = AppleSingleArchiveReader::new(archive)?;
+    let iter = reader.segments();
+    for item in iter {
+        println!("{:?}", &item);
+    }
+    Archive::builder()
+        .format(FORMAT_NAME.into())
+        .build()
+        .ok_or(io::ErrorKind::Other.into())
+}
