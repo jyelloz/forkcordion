851f3c8de424027763789ab81bbcc5c9cf8828da
Jordan Yelloz <jordan@yelloz.me>
2023-01-04 21:48:11 -0700

    main: Updated for latest changes.
    Now it has a stateless handler that streams the resource fork to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use clap::Parser;
 use clio::Input;
+use std::io::Write;
 
 use console::style;
 
-use forkcordion::applesingle;
+use forkcordion::applesingle::{self, Fork};
 
 #[derive(Parser, Debug)]
 #[clap(name = "applesingle-info", author, version, about)]
 struct InfoCommand {
     #[clap(value_parser, default_value = "-")]
     input: Input,
+}
+
+struct Handler;
+
+impl applesingle::Handler for Handler {
+    fn sink(&mut self, fork: applesingle::Fork) -> Option<Box<dyn Write>> {
+        match fork {
+            Fork::Rsrc => {
+                Some(Box::new(std::io::stdout()))
+            }
+            Fork::Data => {
+                eprintln!("data fork");
+                None
+            },
+            Fork::Other(id) => {
+                eprintln!("other fork, id={id}");
+                None
+            }
+        }
+    }
 }
 
 fn main() {
@@ -21,7 +42,9 @@ fn main() {
         style(&cmd.input).yellow(),
     );
 
-    let archive = applesingle::parse(cmd.input)
+    let mut h = Handler;
+
+    let archive = applesingle::parse(cmd.input, &mut h)
         .expect("could not decode as applesingle");
 
     eprintln!("format={}", style(archive.format()).cyan());
