74e79ef1c24abc17eba183992816d3d31e2a7fb1
Jordan Yelloz <jordan@yelloz.me>
2023-01-04 21:45:53 -0700

    archive: Removed forks, added most metadata.
    Not exactly sure what's the best way of writing archive API.
    For seekable input it is pretty straightforward, though.

--- src/archive.rs
+++ src/archive.rs
@@ -1,13 +1,6 @@
-use std::{
-    io::Read,
-    marker::PhantomData,
-};
-
 use derive_more::{From, Into, Display};
 
 use super::{
-    DataFork,
-    ResourceFork,
     FinderInfo,
     MacInfo,
     Filename,
@@ -19,10 +12,8 @@ use super::{
 #[display(fmt = "{}", _0)]
 pub struct Format(&'static str);
 
-pub struct ArchiveBuilder<R> {
+pub struct ArchiveBuilder {
     format: Option<Format>,
-    data: Option<DataFork<R>>,
-    rsrc: Option<ResourceFork<R>>,
     finf: Option<FinderInfo>,
     minf: Option<MacInfo>,
     name: Option<Filename>,
@@ -30,12 +21,10 @@ pub struct ArchiveBuilder<R> {
     comment: Option<Comment>,
 }
 
-impl <R: Read> ArchiveBuilder<R> {
+impl ArchiveBuilder {
     pub fn new() -> Self {
         Self {
             format: None,
-            data: None,
-            rsrc: None,
             finf: None,
             minf: None,
             name: None,
@@ -43,38 +32,71 @@ impl <R: Read> ArchiveBuilder<R> {
             comment: None,
         }
     }
-    pub fn format(mut self, format: Format) -> Self {
+    pub fn format(&mut self, format: Format) -> &Self {
         self.format = Some(format);
         self
     }
-    pub fn build(self) -> Option<Archive<R>> {
+    pub fn finf(&mut self, finf: FinderInfo) -> &Self {
+        self.finf = Some(finf);
+        self
+    }
+    pub fn minf(&mut self, minf: MacInfo) -> &Self {
+        self.minf = Some(minf);
+        self
+    }
+    pub fn date(&mut self, date: Dates) -> &Self {
+        self.date = Some(date);
+        self
+    }
+    pub fn name(&mut self, name: Filename) -> &Self {
+        self.name = Some(name);
+        self
+    }
+    pub fn comment(&mut self, comment: Comment) -> &Self {
+        self.comment = Some(comment);
+        self
+    }
+    pub fn build(&self) -> Option<Archive> {
         let archive = Archive {
             format: self.format?,
             finf: self.finf,
-            _phantom: PhantomData,
+            minf: self.minf,
+            date: self.date,
+            name: self.name.clone(),
+            comment: self.comment.clone(),
         };
         Some(archive)
     }
 }
 
-pub struct Archive<R> {
+#[derive(Debug)]
+pub struct Archive {
     format: Format,
     finf: Option<FinderInfo>,
-    _phantom: PhantomData<R>,
+    minf: Option<MacInfo>,
+    date: Option<Dates>,
+    name: Option<Filename>,
+    comment: Option<Comment>,
 }
 
-impl <R: Read> Archive<R> {
-    pub fn builder() -> ArchiveBuilder<R> {
+impl Archive {
+    pub fn builder() -> ArchiveBuilder {
         ArchiveBuilder::new()
-    }
-    pub fn data_fork(&self) -> Option<DataFork<R>> {
-        None
-    }
-    pub fn rsrc_fork(&self) -> Option<ResourceFork<R>> {
-        None
     }
     pub fn finder_info(&self) -> Option<FinderInfo> {
         self.finf
+    }
+    pub fn mac_info(&self) -> Option<MacInfo> {
+        self.minf
+    }
+    pub fn dates(&self) -> Option<Dates> {
+        self.date
+    }
+    pub fn name(&self) -> Option<Filename> {
+        self.name.clone()
+    }
+    pub fn comment(&self) -> Option<Comment> {
+        self.comment.clone()
     }
     pub fn format(&self) -> Format {
         self.format
